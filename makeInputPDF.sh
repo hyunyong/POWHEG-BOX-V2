06adc789cd9cd81389dff57da5252d7f78bff6ff
hyunyong <antilogy33@gmail.com>
2019-09-05 03:33:32 +0900

    first commit

---
+++ makeInputPDF.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cat dijet_CT10_8TeV.input | sed "s/lhans1  11000.*/#lhans1/ ; s/lhans2  11000.*/#lhans2/;" > powheg.input-PDF-save
+
+for i in {1..53}
+do
+let PDFN=11000+i
+cat powheg.input-PDF-save | sed "s/#lhans1.*/lhans1 $PDFN/ ; s/#lhans2.*/lhans2 $PDFN/ ; " > powheg8TeVPDF$i.input
+
+done
---
+++ makeInputPS.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cat dijet_CT10_8TeV.input | sed "s/renscfact  1d0.*/#renscfact/ ; s/facscfact  1d0.*/#facscfact/;" > powheg.input-PS-save
+
+for i in {1..6}
+do
+
+case $i in
+1) renfac=0.5; facfac=0.5  ;;
+2) renfac=0.5; facfac=1    ;;
+3) renfac=1  ; facfac=0.5  ;;
+4) renfac=1  ; facfac=2    ;;
+5) renfac=2  ; facfac=1    ;;
+6) renfac=2  ; facfac=2    ;;
+esac
+
+cat powheg.input-PS-save | sed "s/#renscfact.*/renscfact $renfac/ ; s/#facscfact.*/facscfact $facfac/ ; " > powhegCase$i.input
+
+done
